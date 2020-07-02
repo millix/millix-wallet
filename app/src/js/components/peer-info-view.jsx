6b0e0f1215c97e40fbf1de3cf4c69fffe4901305
João Almeida <joao.rafael.almeida@ua.pt>
2020-07-02 11:40:05 +0100

    View for node attributes (#7)
    * Create LICENSE
    
    * Create CONTRIBUTING.md
    
    * Create README.md
    
    * Add templates for issue and pull request creation
    
    * Bump jquery from 3.4.1 to 3.5.0 in /app
    
    Bumps [jquery](https://github.com/jquery/jquery) from 3.4.1 to 3.5.0.
    - [Release notes](https://github.com/jquery/jquery/releases)
    - [Commits](https://github.com/jquery/jquery/compare/3.4.1...3.5.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump acorn from 6.3.0 to 6.4.1 in /app
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 6.3.0 to 6.4.1.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/6.3.0...6.4.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump lodash from 4.17.11 to 4.17.13 in /app
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.11 to 4.17.13.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.11...4.17.13)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Fix amount parser
    
    * View for peer info, currently showing only the simple attributes
    
    * Add json information about the node
    
    * Replace _ by spaces
    
    Co-authored-by: Eriksson Monteiro <erikssonmonteiro@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ app/src/js/components/peer-info-view.jsx
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {Button, Col, Row, Table} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {getNodeAttribute} from '../redux/actions/index';
+
+
+class PeerInfoView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    UNSAFE_componentWillMount() {
+        let nodeID = this.props.location.state.peer;
+        this.props.getNodeAttribute(nodeID);
+    }
+
+    render() {
+        let attributes        = this.props.node.attributes;
+        let simpleAttributes  = [];
+        let tabularAttributes = [];
+        attributes.forEach(ele => {
+            if (ele.value instanceof Array) {
+                ele.value.forEach(entry => {
+                    tabularAttributes.push(entry);
+                });
+            }
+            else {
+                let attributeType = ele.attribute_type.replace(/_/g," ");
+                simpleAttributes.push(
+                    <Row className="mb-3"
+                         style={{color: 'lightcyan'}}>
+                        <h5>{attributeType}:</h5>&nbsp;<p>{ele.value}</p>
+                    </Row>
+                );
+            }
+        });
+
+        return (
+            <div>
+                <Row className="mb-3 mt-3">
+                    <Col className="pl-0" style={{
+                        display       : 'flex',
+                        justifyContent: 'flex-start'
+                    }}>
+                        <Button variant="outline-secondary"
+                                onClick={this.props.history.goBack}>
+                            <FontAwesomeIcon icon="arrow-circle-left"
+                                             size="2x"/>
+                            <span style={{
+                                position   : 'relative',
+                                top        : -5,
+                                marginRight: 10,
+                                marginLeft : 10
+                            }}> Back</span>
+                        </Button>
+                    </Col>
+                </Row>
+                {simpleAttributes}
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                    <tr>
+                        <th>shard id</th>
+                        <th>transaction count</th>
+                        <th>update date</th>
+                        <th>is required</th>
+                        <th>fee ask request byte</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tabularAttributes.map((item, idx) => {
+                        return (
+                            <tr key={idx} className="wallet-node">
+                                <td>{item.shard_id}</td>
+                                <td>{item.transaction_count}</td>
+                                <td>{item.update_date}</td>
+                                <td>{item.is_required}</td>
+                                <td>{item.fee_ask_request_byte}</td>
+                            </tr>);
+                    })}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+}
+
+
+export default connect(
+    state => ({
+        node: state.node
+    }),
+    {
+        getNodeAttribute
+    }
+)(withRouter(PeerInfoView));
